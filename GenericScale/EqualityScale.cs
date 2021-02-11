86834e72f9a376bdc2ef950e878fc6735ed3784d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-11 23:27:30 +0200

    Add files via upload

---
+++ GenericScale/EqualityScale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale
+    {
+        public static bool AreEqual<T>(T left, T right)
+        {
+            bool result = false;
+            if(left.Equals(right))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ GenericScale/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //int left = int.Parse(Console.ReadLine());
+            //int right = int.Parse(Console.ReadLine());
+
+            //string left = Console.ReadLine();
+            //string right = Console.ReadLine();
+
+            string[] left = new string[3] { "Ivan", "Kiril", "Alex" };
+            //string[] left = new string[4] { "Ivan", "Kiril", "Alex", "Sophia" };
+            string[] right = new string[4] { "Ivan", "Kiril", "Alex", "Sophia" };
+
+            bool isEquality = EqualityScale.AreEqual(left, right);
+
+            
+
+            Console.WriteLine(isEquality);
+        }
+    }
+}
