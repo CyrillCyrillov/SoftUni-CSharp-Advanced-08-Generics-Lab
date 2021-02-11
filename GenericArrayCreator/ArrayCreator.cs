a88e8e08cd5b188c83e7f94270e73933392ce1c5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-11 22:57:35 +0200

    Add files via upload

---
+++ GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T defoult)
+        {
+            T[] array = new T[length];
+            for (int i = 0; i <= length - 1; i++)
+            {
+                array[i] = defoult;
+            }
+
+            return array;
+        }
+
+    }
+}
