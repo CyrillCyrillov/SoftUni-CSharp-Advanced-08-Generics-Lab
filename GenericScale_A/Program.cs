cd8c0f8a8c4bd49dbb6e50ec1268818d8f6461ba
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-12 00:25:22 +0200

    Add files via upload

---
+++ GenericScale_A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //string[] left = new string[3] { "Ivan", "Kiril", "Alex" };
+            //string[] left = new string[4] { "Ivan", "Kiril", "Alex", "Sophia" };
+            //string[] right = new string[4] { "Ivan", "Kiril", "Alex", "Sophia" };
+
+            EqualityScale<int> scale = new EqualityScale<int>(5, 6);
+            //EqualityScale<int[]> scale = new EqualityScale<int[]>(new int[]{ 1, 2, 5}, new int[]{1, 2, 5 });
+
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
