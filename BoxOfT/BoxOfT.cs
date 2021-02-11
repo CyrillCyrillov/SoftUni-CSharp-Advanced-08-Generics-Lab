c2872ca352ea129d16a41edd81e86c00c2e769d1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-11 22:56:58 +0200

    Add files via upload

---
+++ BoxOfT/BoxOfT.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        Stack<T> boxElements;
+
+        public BoxOfT()
+        {
+            boxElements = new Stack<T>();
+
+        }
+
+        public void Add(T newElement)
+        {
+            boxElements.Push(newElement);
+        }
+
+        public T Remove()
+        {
+            T removedElement = boxElements.Pop();
+            return removedElement;
+        }
+
+        public int Count { get { return boxElements.Count; } }
+    }
+}
